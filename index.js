7acb2775c53dd9d961412ae33c18a5d008c6d8d7
wonllise <wonllise@163.com>
2019-02-20 00:43:11 +0800

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = new Vue({ 
+    // el: '#app',
+    // data: {
+    //     message: 'Hello Vue!'
+    // }
+    el:'#app-5',
+    data: {
+	    message: 'Hello Vue.js!'
+	  },
+	  methods: {
+	    reverseMessage: function () {
+	      this.message = this.message.split('').reverse().join('')
+	    }
+	  }
+});
